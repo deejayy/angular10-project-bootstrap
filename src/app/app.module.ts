6aaa0795b42b164f0c825fe5f2f52c7f886af971
deejayy <deejayy@deejayy.hu>
2020-10-21 23:31:49 +0200

    Setup 09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BemModule } from 'angular-bem';
+
+BemModule.config({
+  separators: ['-', '_', ''],
+  modCase: 'kebab',
+  ignoreValues: false,
+});
 
 @NgModule({
   declarations: [
@@ -12,6 +19,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    BemModule,
     AppRoutingModule,
   ],
   providers: [],
