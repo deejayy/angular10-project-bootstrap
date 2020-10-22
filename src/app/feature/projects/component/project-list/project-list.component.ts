9056a98205d5bcb2a49bafee5d943e2048661cc3
deejayy <deejayy@deejayy.hu>
2020-10-22 23:38:47 +0200

    We have a feature module

---
+++ src/app/feature/projects/component/project-list/project-list.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProjectListComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {}
+}
